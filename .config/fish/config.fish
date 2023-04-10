bac86c44f7404054f2185da061cda12733373fc4
linux_cultist <usual.beach6829@fastmail.com>
2023-04-10 12:41:18 +0200

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # alias gitconfig="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+
+    alias ll="exa -l --icons -snew"
+    alias gs="git status"
+    alias v="nvim"
+    alias vim=v
+    alias c="chezmoi"
+    alias t="tmux"
+    alias ta="t a -t"
+    alias tls="t ls"
+    alias tn="t new -t"
+    alias gitconfig='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+    set fish_greeting
+    set -g fish_autosuggestion_enabled 0
+
+    set -gx DEBUGINFOD_URLS https://debuginfod.archlinux.org/
+    set -gx XDG_CONFIG_DIR $HOME/.config
+    set -gx EDITOR nvim
+
+    fish_add_path /home/cado/.cargo/bin
+
+    # fzf_configure_bindings --variables=
+
+    starship init fish | source
+end
